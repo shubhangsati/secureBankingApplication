e3efdfb60db4313aa73014234c6bebc2ff1c8103
Shubhang Sati <shubhang16198@iiitd.ac.in>
2018-09-26 06:21:59 +0530

    setup0

---
+++ tests.py
@@ -0,0 +1,14 @@
+from app import app
+import unittest
+
+class FlaskTestCase(unittest.TestCase):
+
+    def test_server(self):
+        tester = app.test_client(self)
+        response = tester.get("/test", content_type="html/text")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(b'It works!' in response.data)
+
+
+if __name__ == "__main__":
+    unittest.main()
