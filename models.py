6b72368bdd957c25c13de2e5c816d2a17aeeb690
Shubhang Sati <shubhang16198@iiitd.ac.in>
2018-10-30 16:45:10 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,18 +1,26 @@
-# from app import db
 from flask_cqlalchemy import CQLAlchemy
 import uuid
 
 db = CQLAlchemy()  # create a CQLAlchemy wrapper [uses cassandra-driver]
 
-# Database model for User
-# represents a table in the database
-
 
 class User(db.Model):
-    # Todo: set user id as the primary key
-    # user id
-    uid = db.columns.UUID(default=uuid.uuid4)
-    # Todo: username should be unique, required
-    username = db.columns.Text(primary_key=True, required=True)
-    # password, required
+    uid = db.columns.UUID(primary_key=True, default=uuid.uuid4)
+    username = db.columns.Text(partition_key=True, required=True)
     password = db.columns.Text(required=True)
+
+
+class Account(db.Model):
+    uid = db.columns.Text(required=True)
+    accountNumber = db.columns.Integer(primary_key=True, required=True)
+    balance = db.columns.Integer(required=True)
+    bankBranch = db.columns.Text(required=True)
+
+
+class Transaction(db.Model):
+    transactionType = db.columns.TinyInt(required=True)
+    transactionId = db.columns.Text(primary_key=True, required=True)
+    sourceAC = db.columns.Integer(required=True)
+    destinationAC = db.columns.Integer(required=True)
+    amount = db.columns.Integer(required=True)
+    time = db.columns.DateTime(required=True)
