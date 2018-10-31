973fe78d3b8a2c2ac6b3f2c9292e6331030ba70b
mrinalpaliwal <mrinal16164@iiitd.ac.in>
2018-10-31 17:32:09 +0530

    fix for #29

--- models.py
+++ models.py
@@ -3,11 +3,20 @@ import uuid
 
 db = CQLAlchemy()  # create a CQLAlchemy wrapper [uses cassandra-driver]
 
+class PII(db.Model):
+    uid = db.columns.Text(required=True)
+    email = db.columns.Text(required=True)
+    address = db.columns.Text(required=True)
+    mobile = db.columns.Text(required=True)
+
 
 class User(db.Model):
     uid = db.columns.UUID(primary_key=True, default=uuid.uuid4)
     username = db.columns.Text(partition_key=True, required=True)
     password = db.columns.Text(required=True)
+    #utype can be internal-A, internal-B, internal-C, external-A, or external-B
+    utype = db.columns.Text(required=True,default='internal-A')
+
 
 
 class Account(db.Model):
@@ -24,3 +33,4 @@ class Transaction(db.Model):
     destinationAC = db.columns.Integer(required=True)
     amount = db.columns.Integer(required=True)
     time = db.columns.DateTime(required=True)
+    approvalRequired = db.columns.Boolean(required=True)
