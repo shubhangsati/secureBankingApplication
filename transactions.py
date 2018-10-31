fc482e35b0e80b965cd5f7c8060a17237d417c84
mrinalpaliwal <mrinal16164@iiitd.ac.in>
2018-11-01 00:26:52 +0530

    fixed linting

--- transactions.py
+++ transactions.py
@@ -9,12 +9,11 @@ db.create_keyspace_simple('SBS', 1)  # creates keyspace if does not exist
 
 db.sync_db()
 
-#accepts an object of type Transaction
-#type can be 1, 2 or 3
-#1 is transfer
-#2 is debit, source is BANK for this option
-#3 is credit, source is BANK for this option
-
+# accepts an object of type Transaction
+# type can be 1, 2 or 3
+# 1 is transfer
+# 2 is debit, source is BANK for this option
+# 3 is credit, source is BANK for this option
 
 
 def createTransactionRecord(type, amt, destination, source="BANK"):
@@ -25,15 +24,21 @@ def createTransactionRecord(type, amt, destination, source="BANK"):
     if source != "BANK":
         f2 = Account.objects(accountNumber=destination).count() == 1
     else:
-        f2 = True 
-    if check_amount(amt) and f1 and f2:        
+        f2 = True
+    if check_amount(amt) and f1 and f2:
         flag = True
         critical = False
         if amt >= 100000:
             critical = True
-        
-        transaction = Transaction.create(transactionType=type, sourceAC=source, destinationAC=destination, amount=amt, time=time.asctime(),    approvalRequired=critical)
-        
+
+        transaction = Transaction.create(
+            transactionType=type,
+            sourceAC=source,
+            destinationAC=destination,
+            amount=amt,
+            time=time.asctime(),
+            approvalRequired=critical)
+
         if not critical:
             if type == 1:
                 transfer(transaction)
@@ -45,8 +50,7 @@ def createTransactionRecord(type, amt, destination, source="BANK"):
     return flag
 
 
-
-def tranfer(transaction): 
+def tranfer(transaction):
     src = Account.objects(accountNumber=transaction.sourceAC)[0]
     dst = Account.objects(accountNumber=transaction.destinationAC)[0]
     amt = transaction.amount
@@ -56,14 +60,13 @@ def tranfer(transaction):
         s = src.balance
         d = dst.balance
         if amount <= s:
-                d += amount
-                s -= amount
-                src.update(amount=s)
-                dst.update(amount=d)
-                transaction.completed = True
-    
-    return transaction.completed
+            d += amount
+            s -= amount
+            src.update(amount=s)
+            dst.update(amount=d)
+            transaction.completed = True
 
+    return transaction.completed
 
 
 def debit(transaction):
@@ -74,12 +77,11 @@ def debit(transaction):
     if not completed:
         d = dst.balance
         if amount <= d:
-                d -= amount
-                dst.update(amount=d)
-                transaction.completed = True
-    
-    return transaction.completed
+            d -= amount
+            dst.update(amount=d)
+            transaction.completed = True
 
+    return transaction.completed
 
 
 def credit():
@@ -92,5 +94,5 @@ def credit():
         d += amount
         dst.update(amount=d)
         transaction.completed = True
-    
+
     return transaction.completed
