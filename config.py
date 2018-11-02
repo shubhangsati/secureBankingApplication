43c3b509fb510a6cb3eb9b0bbca6426e1284741d
Shubhang Sati <shubhang16198@iiitd.ac.in>
2018-11-02 20:14:59 +0530

    ..

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class BaseConfig(object):
     SECRET_KEY = "secret_key"  # secret key for database. Todo: change it
     SITE_KEY = "6LcSb3cUAAAAAF8NkmVESlCeODt-7F9qUmYaqKXy"
     CASSANDRA_HOSTS = ['127.0.0.1']  # list of cassandra cluster IP addresses.
-    CASSANDRA_KEYSPACE = 'SBS'  # name of the keyspace (database)
+    CASSANDRA_KEYSPACE = "SBS"  # name of the keyspace (database)
     # PERMANENT_SESSION_LIFETIME = timedelta(minutes=1)
 
 
